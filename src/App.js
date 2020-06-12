bd0253c13c50f622672ab1e65147fa6cf3f31c51
a1oleg <a1olegs@gmail.com>
2020-06-12 08:34:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      allDirs: [{
-        name: 'Мать справочников',
-        values: []
-      }],
+      allDirs: [], //[{name: 'Мать справочников',values: []}],
       actDirs: [],
       results: [],
 
@@ -22,13 +19,15 @@ class App extends Component {
       'bolt://localhost:7687',
       neo4j.auth.basic('neo4j', 'letmein')
     )
-    this.xfetch('Мать справочников', this.state.allDirs); 
+    this.xfetch('Мать справочников', 'allDirs'); 
   }
 
   xfetch = (param, target) => {
-    //console.log(input)
+
+    //this.setState({ allDirs: [...allDirs, {name: param, values: []} ] });
+    
     const session = this.driver.session({ defaultAccessMode: neo4j.session.READ });
-    //const res = [];
+    const res = [];
     session
     .run('MATCH (d{Name: $nameParam})-[:value]->(v) RETURN v.Name', {
       nameParam: param
@@ -38,32 +37,13 @@ class App extends Component {
         //console.log(keys)
       //},
       onNext: record => {
-        //res.push(record._fields[0]);
-        console.log(record._fields[0]);
-        this.setState({ [target]: [target.filter(function(item) { 
-            return item.name === param
-        })[0].values.push(record._fields[0])] });
+        res.push(record._fields[0]);
+        //this.setState({ target: [...target, record._fields[0] ] })
       },
-      onCompleted: () => {    
-        
-        //this.setState({ [target]: [...target, ...res ] });
-        console.log(target)
-        // this.setState(({ allDirs }) => {
-        //   const newArr = allDirs.filter(function(item) { 
-        //       return item === input
-        //   });
-
-        //   newArr.push({
-        //     name: input,
-        //     values: res
-        //   } );    
-        //   return {
-        //     allDirs: newArr
-        //   };
-        // });
-        
-        session.close();// returns a Promise
-        
+      onCompleted: () => {  
+        console.log(this.state)
+         this.setState({ target: res})
+        session.close();// returns a Promise  
       },
       onError: error => {
         console.log(error)
@@ -79,16 +59,11 @@ class App extends Component {
   render() {      
       return (      
       <main> 
-             {this.state.allDirs.filter(function(item) { 
-                return item.values.length > 0 && item.name !== 'Мать справочников'
-            })
-             .map(n => {               
+             {this.state.actDirs.map(n => {               
           return <Selector name= {n.name} values = {n.values}  change ={this.addValue}/>//change ={this.addDir}       
        })}
         
-        <Selector name= {'Справочники'} values={this.state.allDirs.filter(function(item) { 
-                return item.name == 'Мать справочников'
-            })[0].values} change ={this.xfetch}/> 
+        <Selector name= {'Справочники'} values={this.state.allDirs.map(x => x.values)} change ={this.xfetch}/> 
       </main>
     );
   }
